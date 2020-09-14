bc7ed331b692eaa1295f5814bceec39c39cffffd
James Rhead <james.rhead@and.digital>
2020-09-14 14:10:43 +0100

    Update bucket name

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+variable "env" {
+  default = "dev"
+}
+
 resource "aws_s3_bucket" "test" {
-  bucket = "digital.and.james-test123"
+  bucket = "digital.and.james-test123-${var.env}"
+  versioning {
+    enabled = false
+  }
 }
